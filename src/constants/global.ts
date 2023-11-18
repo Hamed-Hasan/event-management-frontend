b1fdd0939ad70d0ff07f54c596c3e2b2c718909a
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-18 13:53:37 +0300

    🚀 Added genderOptions for user selection 🐛 Fixed roleOptions to include correct user roles 🌐 Updated eventStatusOptions for accurate event tracking 🔄 Enhanced statusOptions for booking status management 🛠️ Improved bookingDataFilterOptions for precise data filtering

---
+++ src/constants/global.ts
@@ -0,0 +1,80 @@
+export const genderOptions = [
+  {
+    label: "Male",
+    value: "male",
+  },
+  {
+    label: "Female",
+    value: "female",
+  },
+  {
+    label: "Others",
+    value: "others",
+  },
+];
+
+export const roleOptions = [
+  {
+    label: "SUPER_ADMIN",
+    value: "SUPER_ADMIN",
+  },
+  {
+    label: "ADMIN",
+    value: "ADMIN",
+  },
+  {
+    label: "USER",
+    value: "USER",
+  },
+];
+
+export const eventStatusOptions = [
+  {
+    label: "upcoming",
+    value: "upcoming",
+  },
+  {
+    label: "ongoing",
+    value: "ongoing",
+  },
+  {
+    label: "ended",
+    value: "ended",
+  },
+];
+export const statusOptions = [
+  {
+    label: "pending",
+    value: "pending",
+  },
+  {
+    label: "confirmed",
+    value: "confirmed",
+  },
+  {
+    label: "canceled",
+    value: "canceled",
+  },
+  {
+    label: "completed",
+    value: "completed",
+  },
+];
+export const bookingDataFilterOptions = [
+  {
+    label: "Today",
+    value: "today",
+  },
+  {
+    label: "7 Days",
+    value: "7days",
+  },
+  {
+    label: "1 Month",
+    value: "1month",
+  },
+  {
+    label: "Year",
+    value: "year",
+  },
+];
