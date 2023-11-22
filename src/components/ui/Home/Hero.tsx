603a4ef22bbfe8032c03dbdeedfa7c68f07b2f20
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-22 11:55:43 +0300

    🚀 Implemented dynamic fetching for SwiperSlider content. 🔄 Refactored API endpoint handling in Hero component. 📊 Added loading spinner for better user experience. 🏆 Integrated responsive styles for the hero section. 🌟 Updated event sorting logic in API request.

---
+++ src/components/ui/Home/Hero.tsx
@@ -0,0 +1,38 @@
+import SwiperSlider from "@/components/Slider/SwiperSlider";
+import { baseApi } from "@/config/api";
+import { IApiResponse } from "@/interfaces/apiResponse";
+import styles from "@/styles/Home.module.css";
+import { Spin } from "antd";
+
+const Hero = async () => {
+  const res = await fetch(
+    `${baseApi}/events?limit=${3}&sortBy=startDate&sortOrder=asc`
+  );
+  const result: IApiResponse = await res.json();
+  const data = result?.data;
+
+  if (!data.length) {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Spin size="large" />
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.heroSection}>
+      <div className={`container`}>
+        <SwiperSlider events={data} />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
