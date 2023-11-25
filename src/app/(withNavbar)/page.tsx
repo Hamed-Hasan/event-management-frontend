87cefdc8c96eeec6d87dc5c88ac9f0425dfe7940
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-25 13:28:18 +0300

    🚀 Implemented Hero section with dynamic content. 📊 Added SearchBar component for easy event exploration. 🌟 Enhanced Features section for better user engagement. 🏆 Updated UpcomingEvents to display relevant data. 🔄 Refactored AvailableEvent component for improved modularity.

---
+++ src/app/(withNavbar)/page.tsx
@@ -0,0 +1,29 @@
+import AvailableEvent from "@/components/ui/Home/AvailableEvent";
+import Blogs from "@/components/ui/Home/Blogs";
+import CTA from "@/components/ui/Home/CTA";
+import EventGallery from "@/components/ui/Home/EventGallery";
+import Features from "@/components/ui/Home/Features";
+import Hero from "@/components/ui/Home/Hero";
+import Partners from "@/components/ui/Home/Partners";
+import Reviews from "@/components/ui/Home/Reviews";
+import SearchBar from "@/components/ui/Home/SearchBar";
+import UpcomingEvents from "@/components/ui/Home/UpcomingEvents";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <SearchBar />
+      <Features />
+      <UpcomingEvents />
+      <AvailableEvent />
+      <Partners />
+      <EventGallery />
+      <Blogs />
+      <Reviews />
+      <CTA />
+    </div>
+  );
+};
+
+export default Home;
