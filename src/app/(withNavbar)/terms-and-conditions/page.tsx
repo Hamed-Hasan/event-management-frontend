bb82810f4d7b5401dc30af623b1851155788175f
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-25 13:30:14 +0300

    🚀 Added axios request for fetching Terms and Conditions. 📊 Implemented dynamic page rendering for Privacy Policy. 🌟 Updated metadata for SEO optimization. 🏆 Integrated Spin component for loading state. 🔄 Refactored GEBreadCrumb and PageDetails components.

---
+++ src/app/(withNavbar)/terms-and-conditions/page.tsx
@@ -0,0 +1,55 @@
+import axiosInstance from "@/helpers/axios/axiosInstance";
+import { IApiResponse } from "@/interfaces/apiResponse";
+import { IPage } from "@/interfaces/global";
+import { Spin } from "antd";
+
+import GEBreadCrumb from "@/components/ui/GEBreadCrumb";
+import PageDetails from "@/components/ui/PageDetails";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Terms and Conditions - GreenEcovents",
+  description: "Terms and Conditions of GreenEcovents",
+};
+
+const PrivacyPolicy = async () => {
+  const result = (
+    await axiosInstance.get(`/pages/b80f046f-a109-43c7-b804-ad462bf1c171`)
+  )?.data as IApiResponse;
+  const page: IPage = result?.data;
+
+  if (!page) {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <Spin size="large" />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <GEBreadCrumb
+        title="Terms and Conditions"
+        items={[
+          {
+            label: "Terms and Conditions",
+          },
+        ]}
+      />
+      <div className="container">
+        <PageDetails content={page.content} />
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
