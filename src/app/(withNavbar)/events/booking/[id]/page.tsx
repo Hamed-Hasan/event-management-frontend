d13a32574a3d1fa21abc31c37be221d52dfc767d
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-25 13:45:00 +0300

    🌟 Implemented metadata generation for EventBookingPage. 📈 Updated generateMetadata function for better performance. 🔧 Refactored axiosInstance usage in generateMetadata. 👍 Enhanced error handling in EventBookingPage component. 🚧 Removed unused imports in EventBookingPage.

---
+++ src/app/(withNavbar)/events/booking/[id]/page.tsx
@@ -0,0 +1,46 @@
+import EventBooking from "@/components/ui/Event/EventBooking";
+import axiosInstance from "@/helpers/axios/axiosInstance";
+import { IApiResponse } from "@/interfaces/apiResponse";
+import { IEvent } from "@/interfaces/global";
+import { Metadata, ResolvingMetadata } from "next";
+
+interface IEventBookingProps {
+  params: { id: string };
+}
+
+export async function generateMetadata(
+  { params }: IEventBookingProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const result = (await axiosInstance.get(`/events/${params.id}`))
+    ?.data as IApiResponse;
+  const event: IEvent = result?.data;
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: `Booking of ${event?.title} - GreenEcovents`,
+    description: event?.description,
+    openGraph: {
+      images: [
+        {
+          url: event?.image,
+          width: 800,
+          height: 600,
+          alt: event?.title,
+        },
+        ...previousImages,
+      ],
+    },
+  };
+}
+
+const EventBookingPage = () => {
+  return (
+    <>
+      <EventBooking />
+    </>
+  );
+};
+
+export default EventBookingPage;
