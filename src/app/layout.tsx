0fe598f3b803c62d7e4fdc127be761b01450fb02
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-26 14:51:41 +0300

    🌐 Initialized GreenEcovents RootLayout component. 🎨 Added Google Fonts integration for Montserrat. 📝 Updated metadata for better SEO. 🔄 Refactored and optimized global styles in globals.css file. 🚀 Implemented Providers in RootLayout for state management.

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import Providers from "@/lib/Providers";
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "GreenEcovents - Where Sustainability Meets Celebration",
+  description: "Designed and Developed by Suhag Al Amin",
+  authors: [
+    {
+      name: "Suhag Al Amin",
+      url: "https://suhag.me",
+    },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Providers>
+      <html lang="en">
+        <head></head>
+        <body className={montserrat.className}>{children}</body>
+      </html>
+    </Providers>
+  );
+}
